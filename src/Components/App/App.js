e0909043c6d6e298c179bff05f3941f12d37f900
Jeffrey Lee <jglee1@msn.com>
2020-06-03 21:13:34 -0700

    Feature: completed remove from playlist feature

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
     }
 
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track){
@@ -23,6 +24,13 @@ class App extends React.Component {
       return;
     }
     tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
     this.setState({ playlistTracks: tracks });
   }
 
@@ -36,7 +44,8 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults}
                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
-                     playlistTracks={this.state.playlistTracks} />
+                     playlistTracks={this.state.playlistTracks}
+                     onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
