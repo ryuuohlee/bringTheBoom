31105d897d5b50d7149a66e7e1531dc459e8f67c
Jeffrey Lee <jglee1@msn.com>
2020-06-03 18:18:19 -0700

    Feature: Completed tracklist component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../Playlist/Playlist.css';
+import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
   render() {
     return(
       <div className="Playlist">
         <input defaultValue={"New Playlist"}/>
-        {/*<TrackList />*/}
+        <TrackList />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     )
--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './SearchResults.css';
-//import TrackList;
+import TrackList from '../TrackList/TrackList';
 
 class SearchResults extends React.Component {
   render() {
     return(
       <div className="SearchResults">
         <h2>Results</h2>
-        {/*<TrackList />*/}
+        <TrackList />
       </div>
     )
   }
