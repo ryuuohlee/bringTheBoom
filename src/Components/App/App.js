b1bf30c4bc8f35ce9fa8cd55dec147afbc6d8fa0
Jeffrey Lee <jglee1@msn.com>
2020-06-03 23:28:12 -0700

    Feature: added list renaming feature

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -16,6 +16,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track){
@@ -34,6 +35,10 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
   render() {
     return (
       <div>
@@ -45,7 +50,8 @@ class App extends React.Component {
                            onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
                      playlistTracks={this.state.playlistTracks}
-                     onRemove={this.removeTrack} />
+                     onRemove={this.removeTrack}
+                     onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
